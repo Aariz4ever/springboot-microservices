0ff6a8c13308d9b98ad492aa5fc5e674294fe1c3
Aariz4ever <135448039+Aariz4ever@users.noreply.github.com>
2024-09-18 16:57:21 +0530

    question service

---
+++ question-service/src/main/java/com/aariz/question_service/controller/QuestionController.java
@@ -0,0 +1,64 @@
+package com.aariz.question_service.controller;
+
+
+import com.aariz.question_service.model.Question;
+import com.aariz.question_service.model.QuestionWrapper;
+import com.aariz.question_service.model.Response;
+import com.aariz.question_service.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
+    @Autowired
+    Environment environment;
+
+    @GetMapping("allQuestions")
+    public ResponseEntity<List<Question>> getAllQuestions(){
+        return questionService.getAllQuestions();
+    }
+//    public String getAllQuestions(){
+//        return "Questions";
+//    }
+
+    @GetMapping("category/{category}")
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category){
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("add")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question){
+        return questionService.addQuestion(question);
+    }
+
+    // generate
+
+    @GetMapping("generate")
+    public ResponseEntity<List<Integer>> getQuestionsForQuiz(@RequestParam String categoryName, @RequestParam Integer numQuestions){
+        return questionService.getQuestionsForQuiz(categoryName, numQuestions);
+    }
+    // getQuestions (questionid)
+
+    @PostMapping("getQuestions")
+    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(@RequestBody List<Integer> questionIds){
+        System.out.println(environment.getProperty("local.server.port"));
+        return questionService.getQuestionsFromId(questionIds);
+    }
+
+    // getScore
+
+    @PostMapping("getScore")
+    public ResponseEntity<Integer> getScore(@RequestBody List<Response> responses){
+        return questionService.getScore(responses);
+    }
+
+}
